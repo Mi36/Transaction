2099b05039633774e6a1ee03b36a4ff3661e3c01
Illyas <illyas.deulect@gmail.com>
2022-03-26 17:47:17 +0530

    initialized screens

---
+++ src/screens/TransactionsScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+  fetchTransactionDetails,
+  fetchTransactions,
+} from '../ducks/transactions';
+
+const TransactionsScreen = () => {
+  const dispatch = useDispatch();
+  const transactions = useSelector(state => state);
+
+  React.useEffect(() => {
+    dispatch(fetchTransactions());
+    dispatch(fetchTransactionDetails(1));
+  }, [dispatch]);
+
+  return (
+    <View>
+      <Text>TransactionDetails</Text>
+    </View>
+  );
+};
+
+export default TransactionsScreen;
